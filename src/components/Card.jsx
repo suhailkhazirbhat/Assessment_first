5bade2071e11639b92f5d519a9266a3a627f9b1e
suhailkhazirbhat <suhailbhatb7@gmail.com>
2023-08-27 11:19:35 +0530

    styleCard

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 
 export const Card = props => (
   <Box
+   style={{border:" solid 1px #F6F676",borderRadius:"20px"}}
     bg={useColorModeValue('rgb(214,214,212)', 'gray.700')}
     py='8'
     px={{ base: '4', md: '10' }}
